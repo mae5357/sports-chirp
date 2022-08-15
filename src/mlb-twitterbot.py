5edb4197dfd4b2607e5b65369099c07e576ef9be
Madison Ebersole <madison@perygee.com>
2022-08-15 14:29:08 -0400

    WIP- mlb

--- src/mlb-twitterbot.py
+++ src/mlb-twitterbot.py
@@ -3,35 +3,38 @@ import json
 import os
 from dotenv import load_dotenv
 
+def get_mlb_scores(date='2022-08-11'):
 
-# wrap in function
-def get_epl_scores(date='2022-08-06'):
     # get all the ids of a match for a url 
-    url = "https://v3.football.api-sports.io/fixtures"
+    url = "https://v1.baseball.api-sports.io/games"
 
-    payload={'league':39, 'season':2022, 'date': date}
+    payload={'league':1, 'season':2022,'date':date}
 
     headers = {
         'x-rapidapi-key': os.environ.get("RAPIDAPI_KEY"),
     'x-rapidapi-host': 'v3.football.api-sports.io'
     }
 
-    fixtures_response = json.loads(requests.request("GET", url, headers=headers, params=payload).text)
+    game_response = json.loads(requests.request("GET", url, headers=headers, params=payload).text)
 
-    assert(fixtures_response['errors'] == [])
+    try:
+        assert(game_response['errors'] == [])
+        print('Connected to MLB API')
+    except:
+        print("Error: "+str(game_response['errors']))
 
     score_match_dict = {}
-    for match in fixtures_response['response']:
+    for match in game_response['response']:
         # get the fixture id
-        fixture = match['fixture']['id']
+        game = match['id']
         
         # get the team names
-        home_team = match['teams']['home']['name']
-        away_team = match['teams']['away']['name']
+        home_team = match['teams']['home']['name'].split()[1]
+        away_team = match['teams']['away']['name'].split()[1]
 
         # get the score
-        home_score = match['score']['fulltime']['home']
-        away_score = match['score']['fulltime']['away']
+        home_score = match['scores']['home']['hits']
+        away_score = match['scores']['away']['hits']
 
         # who won?
         if home_score > away_score:
@@ -41,43 +44,44 @@ def get_epl_scores(date='2022-08-06'):
         else:
             winner = 'Draw'
 
-        score_match_dict[fixture]={'home_team':home_team, 'away_team':away_team, 'home_score':home_score, 'away_score':away_score, 'winner':winner}
+        score_match_dict[game]={'home_team':home_team, 'away_team':away_team, 'home_score':home_score, 'away_score':away_score, 'winner':winner}
 
-    # returns a dictionary of the match id and the teams and score
     return score_match_dict
 
-def get_epl_odds(date='2022-08-06'):
-    url = "https://v3.football.api-sports.io/odds"
+def get_mlb_odds(date='2022-08-11'):
+    url = "https://v1.baseball.api-sports.io/odds"
 
-    payload={'league':39, 'season':2022, 'date': date}
+    payload={'league':1, 'season':2022}
 
     headers = {
-        'x-rapidapi-key': os.environ.get("RAPIDAPI_KEY"),
+    'x-rapidapi-key': os.environ.get("RAPIDAPI_KEY"),
     'x-rapidapi-host': 'v3.football.api-sports.io'
     }
 
     odds_response = json.loads(requests.request("GET", url, headers=headers, params=payload).text)
 
-
-    odds_match_dict = {}
+    odds_match_dict={}
     for match in odds_response['response']:
+        if match['game']['id'] in score_match_dict.keys():
+            assert(match['bookmakers'][0]['name']=='NordicBet')
+            assert(match['bookmakers'][0]['bets'][0]['name']=='Match Winner')
 
-        assert(match['bookmakers'][0]['bets'][0]['name'] == 'Match Winner')
+            odd_dict = {}
+            for value in match['bookmakers'][0]['bets'][0]['values']:
+                odd_dict[value['odd']] = value['value']
 
-        odd_dict = {}
-        for value in match['bookmakers'][0]['bets'][0]['values']:
-            odd_dict[value['odd']] = value['value']
-
-        pred_win = odd_dict[min(odd_dict.keys())]
-
-        odds_match_dict[match['fixture']['id']] = pred_win
-
+            pred_win = odd_dict[min(odd_dict.keys())]
+            odds_match_dict[match['game']['id']] = pred_win
+        else:
+            pass
     return odds_match_dict
+
 
 def get_correct_upsets(odds_match_dict,score_match_dict):
     # get lists of correct and incorrect predictions
     correct_ids = []
     upset_ids = []
+
     for match_id in odds_match_dict.keys():
         if odds_match_dict[match_id]!=score_match_dict[match_id]['winner']:
             upset_ids.append(match_id) 
@@ -85,9 +89,8 @@ def get_correct_upsets(odds_match_dict,score_match_dict):
             correct_ids.append(match_id)
     return correct_ids, upset_ids
 
-def create_tweet(correct_ids, upset_ids, score_match_dict, date='2022-08-06'):
-    tweet_text = str('#EPL Matches {date}\n\n').format(date=date)
-    tweet_text=tweet_text+str("UPSETS: \n")
+def create_tweet(correct_ids, upset_ids, score_match_dict, date='2022-08-11'):
+    tweet_text=str("UPSETS: \n")
     for id in upset_ids:
         score_match_dict[id]
         tweet_text=tweet_text+str("{home_team}: {home_score} - {away_team}: {away_score} \n").format(
@@ -105,9 +108,16 @@ def create_tweet(correct_ids, upset_ids, score_match_dict, date='2022-08-06'):
             away_team=score_match_dict[id]['away_team'], 
             away_score=score_match_dict[id]['away_score']))
 
-    return tweet_text
+    if len(tweet_text)>=280:
+        tweet_text_list = tweet_text.split('EXPECTED')
+        tweet_text_list[1]='EXPECTED'+tweet_text_list[1]
+    else:
+        tweet_text_list=[tweet_text]
 
-def tweet_to_twitter(tweet_text):
+    return tweet_text_list
+
+
+def tweet_to_twitter(tweet_text_list, date='2022-08-11'):
     from requests_oauthlib import OAuth1Session
     import os
     import json
@@ -119,8 +129,6 @@ def tweet_to_twitter(tweet_text):
 
     consumer_key =  os.environ.get("TWITTER_CONSUMER_KEY")
     consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
-
-    payload = {"text": tweet_text}
 
     # Get request token
     request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
@@ -157,6 +165,7 @@ def tweet_to_twitter(tweet_text):
     access_token = oauth_tokens["oauth_token"]
     access_token_secret = oauth_tokens["oauth_token_secret"]
 
+
     # Make the request
     oauth = OAuth1Session(
         consumer_key,
@@ -165,32 +174,35 @@ def tweet_to_twitter(tweet_text):
         resource_owner_secret=access_token_secret,
     )
 
-    # Making the request
-    response = oauth.post(
-        "https://api.twitter.com/2/tweets",
-        json=payload,
-    )
+    for tweet in reversed(tweet_text_list):
+        header = 'MLB Scores {date}\n'.format(date=date)
+        payload = {"text": header+tweet}
 
-    if response.status_code != 201:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
+        # Making the request
+        response = oauth.post(
+            "https://api.twitter.com/2/tweets",
+            json=payload,
         )
 
-    print("Response code: {}".format(response.status_code))
+        if response.status_code != 201:
+            raise Exception(
+                "Request returned an error: {} {}".format(response.status_code, response.text)
+            )
 
-    # Saving the response as JSON
-    json_response = response.json()
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+        print("Response code: {}".format(response.status_code))
+
+        # Saving the response as JSON
+        json_response = response.json()
+        print(json.dumps(json_response, indent=4, sort_keys=True))
 
 if __name__ == "__main__":
     load_dotenv()
 
     date = "2022-08-06"
 
-    score_match_dict = get_epl_scores()
-    odds_match_dict = get_epl_odds()
+    score_match_dict = get_mlb_scores()
+    odds_match_dict = get_mlb_odds()
     correct_ids, upset_ids = get_correct_upsets(odds_match_dict,score_match_dict)
-    tweet_text = create_tweet(correct_ids, upset_ids, score_match_dict, date)
-    tweet_to_twitter(tweet_text)
-    print(tweet_text)
-
+    tweet_text_list = create_tweet(correct_ids, upset_ids, score_match_dict, date)
+    tweet_to_twitter(tweet_text_list,date)
+    print(tweet_text_list)
